9439ba52437ddfe81069bf49f249830bf8e91257
Salihov Alexander <salihov1983@gmail.com>
2014-07-14 13:03:05 +0400

    add Logger

---
+++ tests/testTask.cpp
@@ -0,0 +1,12 @@
+#include "..\Task.h"
+#include <string>
+#include <vector>
+#define TESTCASE "Test Task class"
+
+TEST(TESTCASE,"check init func")
+{
+	Task t;
+	std::string number
+
+	t.init();
+}
